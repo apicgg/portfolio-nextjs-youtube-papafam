e3db99294c4b932f4b82844809254c48b48546f2
Anurag Pramanik <p.anurag@outlook.com>
2022-10-04 21:25:53 +0530

    Add Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import Image from 'next/image'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircle from './BackgroundCircle'
+import avatar from '../public/home-right.png'
+import Link from 'next/link'
 
 type Props = {}
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
-    words: [
-      "Hi, I'm Anurag",
-      'nerd-who-loves-coffee.tsx',
-      '<AndCodesWhileDrinkingCoffee />',
-    ],
+    words: ["Hi, I'm Anurag", 'nerd-who-loves-coffee.tsx', '<AndCodes />'],
     loop: true,
     delaySpeed: 1000,
   })
 
   return (
-    <div>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#f7ab0a' />
-      </h1>
+    <div className='flex flex-col h-screen space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircle />
+      <div className='relative rounded-full h-32 w-32 mx-auto object-cover'>
+        <Image src={avatar} alt='Anurag avatar' />
+      </div>
+
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-400 pb-2 tracking-[15px]'>
+          Software Enginner
+        </h2>
+        <h1 className='text-3xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a' />
+        </h1>
+
+        <div className='flex items-center justify-center pt-5'>
+          <Link href='#about'>
+            <div className='heroButton'>About</div>
+          </Link>
+          <Link href='#skills'>
+            <div className='heroButton'>Skills</div>
+          </Link>
+          <Link href='#experience'>
+            <div className='heroButton'>Experience</div>
+          </Link>
+          <Link href='#resume'>
+            <div className='heroButton'>Resume</div>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
